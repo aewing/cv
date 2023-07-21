9fac062b3d0192fa77aea54fadc692e64198d07b
Andrew Ewing <drew@phenocode.com>
2023-07-21 15:48:24 -0700

    page title & meta

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -161,6 +161,18 @@
 	</div>
 </div>
 
+<svelte:head>
+	<title>_drew - staff engineer - fullstack - phoenix, az</title>
+	<meta
+		name="description"
+		content="Andrew Ewing, staff engineer with a focus on fullstack development in Phoenix, AZ."
+	/>
+	<meta
+		name="keywords"
+		content="node.js, typescript, javascript, svelte, sveltekit, developer, engineer, staff engineer, fullstack, phoenix, arizona, az"
+	/>
+</svelte:head>
+
 <style>
 	.container {
 		max-width: 65ch;
