29f63f91799a89944ba961c7ade0b85880f4b561
Andrew Ewing <drew@phenocode.com>
2023-07-21 15:33:35 -0700

    gh pages fix

--- svelte.config.js
+++ svelte.config.js
@@ -7,15 +7,24 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess({})],
 	extensions: ['.svelte', '.md'],
-	paths: process.env.NODE_ENV === 'production' ? { base: '/cv/', assets: '/cv/' } : { base: '' },
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		prerender: {
+			default: true
+		},
+		trailingSlash: 'always',
 		alias: {
 			$data: './src/data'
-		}
+		},
+		paths: process.env.NODE_ENV === 'production' ? { base: '/cv/', assets: '/cv/' } : { base: '' }
 	}
 };
 
